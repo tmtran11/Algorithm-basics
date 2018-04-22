e91c9b4699c36ba37f10a19144560c9ca69ebb68
Trang Tran <tmtran@knox.edu>
2018-04-21 21:00:08 -0500

    Create AStar_puzzle.py

---
+++ algorithm/AStar_puzzle.py
@@ -0,0 +1,197 @@
+import copy
+import time
+
+class Node:
+  def __init__(self,index,d,board,parent,depth):
+    self.index = index
+    self.distance = d
+    self.board = board.copy()
+    self.parent = parent
+    self.depth = depth
+
+class heap():
+  def __init__(self):
+    self.h = []
+    
+  def leftChild(self,index):
+    return index*2+1
+  def rightChild(self,index):
+    return index*2+2
+    
+  def hasLeftChild(self,index):
+    return self.leftChild(index)<len(self.h) and self.leftChild(index)>-1
+  def hasRightChild(self,index):
+    return self.rightChild(index)<len(self.h) and self.rightChild(index)>-1
+    
+  def isLeaf(self,index):
+    return not self.hasRightChild(index) and not self.hasLeftChild(index)
+  def parent(self,index):
+    return (index-1)//2
+    
+  def remove_head(self):
+    head = copy.copy(self.h[0])
+    self.h[0] = self.h[-1]
+    self.h = self.h[:-1]
+    self.down_heapify(0)
+    return head
+    
+  def down_heapify(self,index):
+    if self.isLeaf(index):
+      return
+    if self.hasLeftChild(index) and self.h[self.leftChild(index)].distance<self.h[index].distance:
+      if (self.hasRightChild(index) and self.h[self.leftChild(index)].distance<self.h[self.rightChild(index)].distance) or not self.hasRightChild(index):
+          self.h[index], self.h[self.leftChild(index)] = self.h[self.leftChild(index)], self.h[index]
+          self.down_heapify(self.leftChild(index))
+    if self.hasRightChild(index) and self.h[self.rightChild(index)].distance<self.h[index].distance:
+      if (self.hasLeftChild(index) and self.h[self.rightChild(index)].distance<self.h[self.leftChild(index)].distance) or not self.hasLeftChild(index):
+        self.h[index], self.h[self.rightChild(index)] = self.h[self.rightChild(index)], self.h[index]
+        self.down_heapify(self.rightChild(index))
+    return
+  
+  def add(self,node):
+    self.h.append(node)
+    self.up_heapify(len(self.h)-1)
+    # print([x.distance for x in self.h])
+    
+  def up_heapify(self,index):
+    if index==0:
+      return
+    if self.h[index].distance<self.h[self.parent(index)].distance:
+      self.h[index], self.h[self.parent(index)] = self.h[self.parent(index)], self.h[index]
+      self.up_heapify(self.parent(index))
+    return
+
+def distance(board):
+  d = 0
+  for n,x in enumerate(board):
+    diff = abs(x-n)
+    if x//3==n//3:
+      d += diff
+    else:
+      d += diff//3 + abs(diff//3*3-diff)
+  return d
+  
+def dist(x,n):
+  d = 0
+  diff = abs(x-n)
+  if x//3==n//3:
+    d += diff
+  else:
+    d += diff//3 + abs(diff//3*3-diff)
+  return d
+  
+def delta_d(board, a, b):
+  return dist(board[a],b)-dist(board[a],a)+dist(board[b],a)-dist(board[b],b)
+  
+def test_heap():
+  h = heap()
+  h.add(Node(0,10,[],None,1))
+  h.add(Node(0,12,[],None,1))
+  h.add(Node(0,1,[],None,1))
+  h.add(Node(0,9,[],None,1))
+  h.add(Node(0,0,[],None,1))
+  print(h.remove_head().distance)
+  print(h.remove_head().distance)
+  print(h.remove_head().distance)
+  print(h.remove_head().distance)
+  
+def trace(node):
+  d = {-3:"Up",3:"Down",-1:"Left",1:"Right"}
+  path_to_goal = []
+  cost_of_path = 0
+  while node.parent!=None:
+    path_to_goal.append(d[node.index-node.parent.index])
+    cost_of_path += 1
+    node = node.parent
+  return path_to_goal[::-1], cost_of_path
+  
+def printf(board):
+  print(str(board[0])+" "+str(board[1])+" "+str(board[2])+'\n')
+  print(str(board[3])+" "+str(board[4])+" "+str(board[5])+'\n')
+  print(str(board[6])+" "+str(board[7])+" "+str(board[8])+'\n')
+
+def ast(board):
+  start_time = time.time()
+  
+  frontier = heap()
+  d = distance(board)
+  node = Node(board.index(0),d,board,None,1)
+  frontier.add(node)
+  result = None
+  
+  nodes_expanded = 1
+  max_search_depth = 1
+  while 1:
+    node = frontier.remove_head()
+    index = node.index
+    board = node.board.copy()
+    depth = node.depth
+    d = node.distance
+    
+    print("distance: "+str(d))
+    #print("child: \n")
+    #printf(board)
+    
+    if(node.parent!=None):
+      indexP = node.parent.index
+    else:
+      indexP=-1
+    prev_board = board.copy()
+    
+    if index-3>0 and not index-3==indexP:
+      td = d+delta_d(board,index,index-3)
+      board[index],board[index-3] = board[index-3],board[index]
+      if board==[0,1,2,3,4,5,6,7,8]:
+        result = Node(index-3,td,board,node,depth+1)
+        break
+      frontier.add(Node(index-3,td,board,node,depth+1))
+      nodes_expanded+=1
+      board = prev_board.copy()
+      
+    if index+3<9 and not index+3==indexP:
+      td = d+delta_d(board,index,index+3)
+      board[index],board[index+3] = board[index+3],board[index]
+      if board==[0,1,2,3,4,5,6,7,8]:
+        result = Node(index+3,td,board,node,depth+1)
+        break
+      frontier.add(Node(index+3,td,board,node,depth+1))
+      nodes_expanded+=1
+      board = prev_board.copy()
+      
+    if index//3==(index-1)//3 and not index-1==indexP:
+      td = d+delta_d(board, index, index-1)
+      board[index],board[index-1] = board[index-1],board[index]
+      if board==[0,1,2,3,4,5,6,7,8]:
+        result = Node(index-1,td,board,node,depth+1)
+        break
+      frontier.add(Node(index-1,td,board,node,depth+1))
+      nodes_expanded+=1
+      board = prev_board.copy()
+      
+    if index//3==(index+1)//3 and not index+1==indexP:
+      td = d+delta_d(board, index, index+1)
+      board[index],board[index+1] = board[index+1],board[index]
+      if board==[0,1,2,3,4,5,6,7,8]:
+        result = Node(index+1,td,board,node,depth+1)
+        break
+      frontier.add(Node(index+1,td,board,node,depth+1))
+      nodes_expanded+=1
+      board = prev_board.copy()
+    if depth+1>max_search_depth:
+      max_search_depth = depth+1
+  
+  if depth+1>max_search_depth:
+      max_search_depth = depth+1
+  path_to_goal,cost_of_path = trace(result)
+  search_depth = result.depth
+  
+  print("path_to_goal: ",path_to_goal)
+  print("cost_of_path: ",cost_of_path)
+  print("nodes_expanded: ",nodes_expanded)
+  print("search_depth: ", search_depth)
+  print("max_search_depth: ", max_search_depth)
+  print("running_time: ",time.time()-start_time)
+
+#test = [6,1,8,4,0,2,7,3,5]
+test = [1,0,2,3,5,8,6,7,4]
+ast(test)
